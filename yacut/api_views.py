9066a5ef1ecf03afa127a7a61c27b3d3376cc067
vovanbart <vovanbart@icloud.com>
2022-10-26 22:27:44 +0300

    api_views

--- yacut/api_views.py
+++ yacut/api_views.py
@@ -5,14 +5,15 @@ from .constants import MATCH
 from .error_handlers import InvalidAPIUsage
 from .models import URL_map
 from .utils import get_unique_short_id, check_original
+from http import HTTPStatus
 
 
 @app.route('/api/id/<string:short_id>/', methods=('GET',))
 def get_link(short_id):
     target: str = check_original(short_id)
     if not target:
-        raise InvalidAPIUsage('Указанный id не найден', 404)
-    return jsonify({'url': target}), 200
+        raise InvalidAPIUsage('Указанный id не найден', HTTPStatus.NOT_FOUND)
+    return jsonify({'url': target}), HTTPStatus.OK
 
 
 @app.route('/api/id/', methods=('POST',))
@@ -21,20 +22,20 @@ def push_link():
     if not data:
         raise InvalidAPIUsage('Отсутствует тело запроса')
 
-    elif 'url' not in data:
+    if 'url' not in data:
         raise InvalidAPIUsage('"url" является обязательным полем!')
 
-    elif not data.get('custom_id'):
+    if not data.get('custom_id'):
         data.update({'custom_id': get_unique_short_id()})
 
-    elif not MATCH.search(data['custom_id']):
+    if not MATCH.search(data['custom_id']):
         raise InvalidAPIUsage('Указано недопустимое имя для короткой ссылки')
 
-    elif check_original(data['custom_id']):
+    if check_original(data['custom_id']):
         raise InvalidAPIUsage(f'Имя "{data["custom_id"]}" уже занято.')
 
-    short: URL_map = URL_map()
+    short = URL_map()
     short.from_dict(data)
     db.session.add(short)
     db.session.commit()
-    return jsonify(short.to_dict()), 201
+    return jsonify(short.to_dict()), HTTPStatus.CREATED
