70453fe091163640d2dc222e7aaab963d6a8620e
vovanbart <vovanbart@icloud.com>
2022-10-27 20:54:30 +0300

    1234567

--- yacut/models.py
+++ yacut/models.py
@@ -9,7 +9,9 @@ class URL_map(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     original = db.Column(db.String(256), nullable=False)
     short = db.Column(db.String(16))
-    timestamp = db.Column(db.DateTime, default=datetime.utcnow())
+    timestamp = db.Column(db.DateTime,
+                          default=datetime.utcnow(),
+                          onupdate=datetime.now)
 
     def to_dict(self):
         value = dict(
